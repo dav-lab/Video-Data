2209b2abbcf435a522e2fca4cdff5137dbbd0772
dav-lab <emustafa-local@sci-160-12-a.local>
2015-06-25 16:49:30 -0400

    Commented Code in videoTranscripts
    Added comments to videoTranscript.
    Also created dictionary for videoIDs and transcript

--- videoTranscripts.py
+++ videoTranscripts.py
@@ -7,45 +7,43 @@ import bisect
 import os
 import isodate
 from xml.parsers.expat import ExpatError
-import pprint
+import pprint #for pretty printing
 
 APIKEY='AIzaSyDKoeFuf8lF9bO3cQasg5MSf6SDjgBjDgc'
 videoReader = open('videos.json').read()
 videoLoader = json.loads(videoReader)
 videoIDs =[]
-for i in videoLoader:
+
+for i in videoLoader: #appends videoIDs to the list videoIDs
     videoIDs.append(i)
 
-transcriptDict = {}
-transcriptList = []
-for VIDEOID in videoIDs:
-        #print VIDEOID
-    #VIDEOID = 'BWBUXxyRILw'
+transcriptDict = {} #Dictionary for videoIDs and transcript of video
+
+for VIDEOID in videoIDs: #Goes through individual videoIDs in the list
     info=json.loads(requests.get('https://www.googleapis.com/youtube/v3/videos?part=contentDetails%2Cstatistics&id='+VIDEOID+'&key='+APIKEY).content)
-    #vidLength=info['items'][0]['contentDetails']['duration']
-    try:    
-        if info['items'][0]['contentDetails']['caption']=='true':
+    try:
+        if info['items'][0]['contentDetails']['caption']=='true': #checks if the video has the caption(transcript) info
             subs=requests.get('http://video.google.com/timedtext?lang=en&v='+VIDEOID).content
             a=xmltodict.parse(subs)
-            listOfSubs=a['transcript']['text']
-            
-            #while loop traverses listOfSubs(not including first and last element because they don't have text)
-            #listOfSubs[i].items()[2][1] accesses ordered dict (like a tuple)
+            listOfSubs=a['transcript']['text']   
+            transcriptList = []
             i = 1
-            while i <  len(listOfSubs)-1:
-                transcript = listOfSubs[i].items()[2][1]
+            while i <  len(listOfSubs)-1: #while loop traverses listOfSubs(not including first and last element because they don't have text)
+                transcript = listOfSubs[i].items()[2][1] #listOfSubs[i].items()[2][1] accesses ordered dict
                 newTranscript = transcript.replace("&#39;","'")
                 transcriptList.append(newTranscript)
-                i += 1
-                
-            #print '' 
+                i += 1          
             transcriptDict[VIDEOID] = [transcriptList]
-            
-            
+   
     except (IndexError, ExpatError), e:
         #print 'indexError on ',VIDEOID
         transcriptDict[VIDEOID] = ['NTA']
-
+        
+def printDict():
+    pprint.pprint(transcriptDict) #pretty prints the transcriptDict   
+    
+def wordFreqCounter():
+    
 
 #
 #Code for looking at transcript of one video
